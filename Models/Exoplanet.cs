abab9ce469257f5373bb916e66aca0c6f9e66a2d
Ravindu Hettiarachchi <ravindu.hettiarachchi@ifs.com>
2025-03-06 18:50:47 +0530

    Add project files.

---
+++ Models/Exoplanet.cs
@@ -0,0 +1,14 @@
+﻿namespace ExoplanetQueryApp.Models
+{
+    public class Exoplanet
+    {
+        public string PlanetName { get; set; }       // pl_name
+        public string HostName { get; set; }         // hostname
+        public string DiscoveryMethod { get; set; }  // discoverymethod
+        public int? DiscoveryYear { get; set; }      // disc_year
+        public double? OrbitalPeriod { get; set; }   // pl_orbper
+        public double? PlanetRadius { get; set; }    // pl_rade
+        public double? PlanetMass { get; set; }      // pl_masse
+        public double? Distance { get; set; }        // sy_dist
+    }
+}
---
+++ Services/ExoplanetService.cs
@@ -0,0 +1,19 @@
+﻿using ExoplanetQueryApp.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+namespace ExoplanetQueryApp.Services
+{
+    public class ExoplanetService
+    {
+        private readonly List<Exoplanet> _exoplanets;
+
+        public ExoplanetService() {
+            _exoplanets = LoadExoplanetData();
+    }
+
+        private List<Exoplanet> LoadExoplanetData()
+        {
+
+        }
+}
